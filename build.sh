67cbb24aa35c107398b79603858abc086dda26f6
chiabre <luca.chiabrera@moviri.com>
2021-03-22 16:33:08 +0100

    new dockerfile

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-JAVA_VERSION="11"
 RENAISSANCE_VERSION="0.11.0"
 JMX_EXPORTER_VERSION="0.15.0"
 
-docker build --network host --build-arg JAVA_VERSION=${JAVA_VERSION} --build-arg RENAISSANCE_VERSION=${RENAISSANCE_VERSION} --build-arg JMX_EXPORTER_VERSION=${JMX_EXPORTER_VERSION} -t "chiabre/renaissance_jmx_exporter:${RENAISSANCE_VERSION}-java${JAVA_VERSION}" .
-# docker build  --build-arg JAVA_VERSION=${JAVA_VERSION} --build-arg JAVA_FLAVOR=-hotspot --build-arg RENAISSANCE_VERSION=${RENAISSANCE_VERSION} --build-arg JMX_EXPORTER_VERSION=${JMX_EXPORTER_VERSION} -t "chiabre/renaissance_jmx_exporter:${RENAISSANCE_VERSION}-java${JAVA_VERSION}-hotspot" .
-# docker build  --build-arg JAVA_VERSION=${JAVA_VERSION} --build-arg JAVA_FLAVOR=-openj9 --build-arg RENAISSANCE_VERSION=${RENAISSANCE_VERSION} --build-arg JMX_EXPORTER_VERSION=${JMX_EXPORTER_VERSION} -t "chiabre/renaissance_jmx_exporter:${RENAISSANCE_VERSION}-java${JAVA_VERSION}-openj9" .
+for JAVA_VERSION in 8 11 12 13 14 15 
+do
+    for JAVA_JVM in "" "-hotspot" "-openj9"
+    do
+        docker build --build-arg JAVA_VERSION=${JAVA_VERSION} --build-arg JAVA_JVM=${JAVA_JVM}  --build-arg RENAISSANCE_VERSION=${RENAISSANCE_VERSION} --build-arg JMX_EXPORTER_VERSION=${JMX_EXPORTER_VERSION} -t "chiabre/renaissance_jmx_exporter:${RENAISSANCE_VERSION}-java${JAVA_VERSION}${JAVA_JVM}" .
+    done
+done
