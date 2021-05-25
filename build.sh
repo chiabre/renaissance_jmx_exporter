2001e11a78ffcdf27792bdd4aca3144da1e49f0f
chiabre <luca.chiabrera@moviri.com>
2021-05-25 14:57:31 -0400

    build & push

--- build.sh
+++ build.sh
@@ -8,5 +8,6 @@ do
     for JAVA_JVM in "" "-openj9"
     do
         docker build --build-arg JAVA_VERSION=${JAVA_VERSION} --build-arg JAVA_JVM=${JAVA_JVM}  --build-arg RENAISSANCE_VERSION=${RENAISSANCE_VERSION} --build-arg JMX_EXPORTER_VERSION=${JMX_EXPORTER_VERSION} -t "chiabre/renaissance_jmx_exporter:${RENAISSANCE_VERSION}-java${JAVA_VERSION}${JAVA_JVM}" .
+        docker push "chiabre/renaissance_jmx_exporter:${RENAISSANCE_VERSION}-java${JAVA_VERSION}${JAVA_JVM}"
     done
 done
